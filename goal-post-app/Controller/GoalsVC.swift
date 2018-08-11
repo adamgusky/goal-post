c6a736d1a1696fbf3151d79fb9859f680b230a5e
Adam Gusky <gusky.adam@uwlax.edu>
2018-08-11 08:32:38 -0400

    first commit

---
+++ goal-post-app/Controller/GoalsVC.swift
@@ -0,0 +1,25 @@
+//
+//  GoalsVC.swift
+//  goal-post-app
+//
+//  Created by Adam Gusky on 8/11/18.
+//  Copyright © 2018 Adam Gusky. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
